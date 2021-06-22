02cf485ee18e1431f8a04e1fb7ae8298fa306548
kuskovan <techinek@gmail.com>
2021-06-22 12:30:34 +0300

    Add view and template for the main page

--- src/blog_drf_js/urls.py
+++ src/blog_drf_js/urls.py
@@ -18,9 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from posts.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/posts/', include('posts.urls')),
+    path('', home, name='home'),
 ]
 
 if settings.DEBUG:
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,15 +1,24 @@
+from django.shortcuts import render
 from rest_framework.generics import (
- ListAPIView,
-RetrieveAPIView,
-CreateAPIView,
-UpdateAPIView,
-DestroyAPIView
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    UpdateAPIView,
+    DestroyAPIView
 )
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
 from .models import Author, Post
 from .serializers import PostSerializer
 
+
+def home(request):
+    return render(request, 'index.html')
+
+
 class PostListView(ListAPIView):
+
+    """View to render posts"""
     permission_classes = (AllowAny,)
     serializer_class = PostSerializer
     queryset = Post.objects.all()
